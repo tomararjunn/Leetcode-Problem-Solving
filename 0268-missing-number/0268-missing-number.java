8c9aed781c5d8cb4e3ae4bcb441c6a487ba25c18
ARJUN SINGH TOMAR <arjuntomar9955@gmail.com>
2026-08-02 19:26:45 +0530

    Time: 0 ms (100%), Space: 47.2 MB (66.61%) - LeetHub

---
+++ 0268-missing-number/0268-missing-number.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int missingNumber(int[] nums) {
+        int n=nums.length;
+        int xor=n;
+        for(int i=0;i<n;i++){
+            xor=xor^i^nums[i];
+        }
+        return xor;
+    }
+}
