a963cb5d8ae5ad98b9b85ec091f64e6d1110bc2c
ARJUN SINGH TOMAR <arjuntomar9955@gmail.com>
2026-08-12 00:15:08 +0530

    Time: 0 ms (100%), Space: 47.6 MB (69.21%) - LeetHub

---
+++ 0042-trapping-rain-water/0042-trapping-rain-water.java
@@ -0,0 +1,33 @@
+class Solution {
+    public int trap(int[] height) {
+
+        //declare
+        int left=0;
+        int right=height.length-1;
+        int leftmax=0;
+        int rightmax=0;
+        int water=0;
+        
+        //while loop with condition
+        while(left<right){
+            if(height[left]<height[right]){
+                if(height[left]>=leftmax){
+                    leftmax=height[left];
+                }
+                else{
+                    water+=leftmax-height[left];
+                }
+                left++;
+            }else{
+                if(height[right]>=rightmax){
+                    rightmax=height[right];
+                }
+                else{
+                    water+=rightmax-height[right];
+                }
+                right--;
+            }
+        }
+        return water;
+    }
+}
