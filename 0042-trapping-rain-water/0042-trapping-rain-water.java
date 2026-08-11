e21a4795f9da28fdda34b230712760a2e46e85af
ARJUN SINGH TOMAR <arjuntomar9955@gmail.com>
2026-08-12 00:24:37 +0530

    Time: 0 ms (100%), Space: 47.7 MB (56.72%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.java
+++ 0042-trapping-rain-water/0042-trapping-rain-water.java
@@ -1,44 +1,41 @@
 class Solution {
     public int trap(int[] height) {
 
-        int left = 0;
-        int right = height.length - 1;
-
-        int leftmax = 0;   // Highest wall seen from left
-        int rightmax = 0;  // Highest wall seen from right
-        int water = 0;     // Total trapped water
-
-        while (left < right) {
-
+        //declare
+        int left=0;
+        int right=height.length-1;
+        int leftmax=0;
+        int rightmax=0;
+        int water=0;
+        
+        //while loop with condition
+        while(left<right){
             // Process the shorter side
-            if (height[left] < height[right]) {
 
+            if(height[left]<height[right]){
+    
                 // New highest left wall
-                if (height[left] >= leftmax) {
-                    leftmax = height[left];
-                } 
-                else {
+                if(height[left]>=leftmax){
+                    leftmax=height[left];
+                }
+                else
+                {
                     // Water = left wall - current height
-                    water += leftmax - height[left];
+                    water+=leftmax-height[left];
                 }
 
                 left++;
-
-            } else {
-
+            }else{
                 // New highest right wall
-                if (height[right] >= rightmax) {
-                    rightmax = height[right];
-                } 
-                else {
-                    // Water = right wall - current height
-                    water += rightmax - height[right];
+                if(height[right]>=rightmax){
+                    rightmax=height[right];
                 }
-
+                else{
+                    water+=rightmax-height[right];
+                }
                 right--;
             }
         }
-
         return water;
     }
 }
