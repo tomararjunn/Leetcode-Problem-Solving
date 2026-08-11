d8bf9c4dcb7d8bbd5e75bb3e5e814eef4cdf129d
ARJUN SINGH TOMAR <arjuntomar9955@gmail.com>
2026-08-12 00:24:00 +0530

    Time: 1 ms (58.74%), Space: 47.5 MB (91.96%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.java
+++ 0042-trapping-rain-water/0042-trapping-rain-water.java
@@ -1,33 +1,44 @@
 class Solution {
     public int trap(int[] height) {
 
-        //declare
-        int left=0;
-        int right=height.length-1;
-        int leftmax=0;
-        int rightmax=0;
-        int water=0;
-        
-        //while loop with condition
-        while(left<right){
-            if(height[left]<height[right]){
-                if(height[left]>=leftmax){
-                    leftmax=height[left];
+        int left = 0;
+        int right = height.length - 1;
+
+        int leftmax = 0;   // Highest wall seen from left
+        int rightmax = 0;  // Highest wall seen from right
+        int water = 0;     // Total trapped water
+
+        while (left < right) {
+
+            // Process the shorter side
+            if (height[left] < height[right]) {
+
+                // New highest left wall
+                if (height[left] >= leftmax) {
+                    leftmax = height[left];
+                } 
+                else {
+                    // Water = left wall - current height
+                    water += leftmax - height[left];
                 }
-                else{
-                    water+=leftmax-height[left];
-                }
+
                 left++;
-            }else{
-                if(height[right]>=rightmax){
-                    rightmax=height[right];
+
+            } else {
+
+                // New highest right wall
+                if (height[right] >= rightmax) {
+                    rightmax = height[right];
+                } 
+                else {
+                    // Water = right wall - current height
+                    water += rightmax - height[right];
                 }
-                else{
-                    water+=rightmax-height[right];
-                }
+
                 right--;
             }
         }
+
         return water;
     }
 }
