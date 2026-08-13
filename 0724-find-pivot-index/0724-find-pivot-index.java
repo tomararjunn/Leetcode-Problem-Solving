2c8433338e384536f88977a90e533af29f46c5e2
ARJUN SINGH TOMAR <arjuntomar9955@gmail.com>
2026-08-13 20:03:04 +0530

    Time: 1 ms (97.53%), Space: 47.4 MB (40.89%) - LeetHub

--- 0724-find-pivot-index/0724-find-pivot-index.java
+++ 0724-find-pivot-index/0724-find-pivot-index.java
@@ -1,14 +1,19 @@
 class Solution {
     public int pivotIndex(int[] nums) {
-        int n=nums.length;
+        //first cal total sum
         int totalsum=0;
         for(int x:nums){
             totalsum+=x;
         }
+
+
+        // Calculate left and right sum
         int leftsum=0;
-        for(int i=0;i<n;i++){
+        for(int i=0;i<nums.length;i++){
+            
             int rightsum=totalsum-leftsum-nums[i];
             if(leftsum==rightsum){
+
                 return i;
             }
             leftsum+=nums[i];
