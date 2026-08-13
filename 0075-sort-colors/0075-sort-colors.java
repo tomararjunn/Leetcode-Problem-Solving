3f2accbbbd63c5c390ac5cff941b1eed922f951b
ARJUN SINGH TOMAR <arjuntomar9955@gmail.com>
2026-08-13 22:32:12 +0530

    Time: 0 ms (100%), Space: 43.5 MB (60.68%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.java
+++ 0075-sort-colors/0075-sort-colors.java
@@ -2,21 +2,21 @@ public class Solution {
     public static void sortColors(int[] nums) {
 
 
-        int low = 0;
+        int left = 0;
         int mid = 0;
-        int high = nums.length - 1;
+        int right = nums.length - 1;
 
 
-        while(mid <= high) {
+        while(mid <= right) {
 
             if(nums[mid] == 0) {
 
-                // swap low and mid
-                int temp = nums[low];
-                nums[low] = nums[mid];
+                // swap left and mid
+                int temp = nums[left];
+                nums[left] = nums[mid];
                 nums[mid] = temp;
 
-                low++;
+                left++;
                 mid++;
             }
 
@@ -28,12 +28,12 @@ public class Solution {
 
             else { // nums[mid] == 2
 
-                // swap mid and high
+                // swap mid and right
                 int temp = nums[mid];
-                nums[mid] = nums[high];
-                nums[high] = temp;
+                nums[mid] = nums[right];
+                nums[right] = temp;
 
-                high--;
+                right--;
             }
         }
     }
