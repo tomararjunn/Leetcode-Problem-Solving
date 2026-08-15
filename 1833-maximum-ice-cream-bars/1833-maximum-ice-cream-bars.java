7957209824b2d386ce06f7f91566f90695c99b0d
ARJUN SINGH TOMAR <arjuntomar9955@gmail.com>
2026-08-15 23:33:04 +0530

    Time: 37 ms (47.94%), Space: 84.1 MB (27.32%) - LeetHub

---
+++ 1833-maximum-ice-cream-bars/1833-maximum-ice-cream-bars.java
@@ -0,0 +1,16 @@
+import java.util.Arrays;
+class Solution {
+    public int maxIceCream(int[] costs, int coins) {
+        Arrays.sort(costs);
+        int count=0;
+        for(int i=0;i<costs.length;i++){
+            if(costs[i]>coins){
+                break;
+            }
+            coins-=costs[i];
+            count++;
+        }
+        return count;
+    }
+}
+          
